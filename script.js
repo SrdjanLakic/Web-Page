f814cf8fe819720b16747681ecfd9792693587e7
SrdjanLakic <srdjan949@gmail.com>
2022-07-24 21:14:01 -0400

    updated

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const timeLine = gsap.timeline({ defaults: { ease: 'power1.out' } });
 
 timeLine.to('.text', { y: '0%', duration: 2, stagger: 0.25 });
-timeLine.to('.slider', { y: '-100%', duration: 2, delay: 2 });
+timeLine.to('.slider', { y: '-100%', duration: 2, delay: 1 });
 timeLine.to('.intro', { y: '-100%', duration: 1 }, '-=2');
 timeLine.fromTo('nav', { opacity: 0 }, { opacity: 1, duration: 1 });
 timeLine.fromTo(
