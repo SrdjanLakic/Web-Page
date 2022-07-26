d3e6c8e41df1f00ec4fb354da9e2c81a4cfd71dc
SrdjanLakic <srdjan949@gmail.com>
2022-07-26 17:19:21 -0400

    sticky header added

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const timeLine = gsap.timeline({ defaults: { ease: 'power1.out' } });
+const header = document.querySelector('header');
+const section1 = document.querySelector('#section--1');
+const nav = document.querySelector('.nav');
 
 timeLine.to('.text', { y: '0%', duration: 2, stagger: 0.25 });
 timeLine.to('.slider', { y: '-100%', duration: 2, delay: 1 });
@@ -10,3 +13,16 @@ timeLine.fromTo(
   { opacity: 1, duration: 1 },
   '-=1'
 );
+
+const initialCoords = header.getBoundingClientRect();
+console.log(initialCoords.top);
+window.addEventListener('scroll', function () {
+  console.log(window.scrollY);
+  if (window.scrollY > initialCoords.bottom) {
+    nav.classList.add('sticky');
+
+    nav.style.width = '100%';
+  } else {
+    nav.classList.remove('sticky');
+  }
+});
