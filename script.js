f5cc6eb63190071e3cfd869c4df69cf3a497f650
SrdjanLakic <srdjan949@gmail.com>
2022-08-05 18:00:41 -0400

    unobserver implemented

--- script.js
+++ script.js
@@ -52,6 +52,7 @@ const revealSection = function (entries, observer) {
   console.log(entry);
   if (!entry.isIntersecting) return;
   entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
 };
 
 const observer = new IntersectionObserver(revealSection, {
